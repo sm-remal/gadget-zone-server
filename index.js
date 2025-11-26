6bf4c011ea9d80dbb40ff76ba08c7f63c6d7b140
sm-remal <remal.mozzem@gmail.com>
2025-11-26 20:24:43 +0600

    added varcel.json

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 // ========= Connection set function ========== //
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         // ========== MAIN FUNCTION ========== //
 
@@ -114,7 +114,7 @@ async function run() {
         });
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
